9adf4e29267bfb6713660e145c4d5db7a6d7c479
Krystian Ogonowski <tailowskikrystian@gmail.com>
2022-04-07 00:55:52 +0200

    corrected that init
    - front directory
    - paradigm graphs

--- back/src/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
-
-
-class SurveySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password')
